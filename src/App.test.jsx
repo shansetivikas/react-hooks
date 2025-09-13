da3d0208c473ee7e726edeaee10ce3fc9ac0d3bd
shansetivikas <vikas.shetty.456@gmail.com>
2025-09-13 20:43:27 +0530

    updated test cases

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { expect, test } from 'vitest';
+
+test('renders hello text', () => {
+  render(<App />);
+  expect(1+2).toBe(3)
+});
